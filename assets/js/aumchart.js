143b3f38cab98a78a189f2ce11f47bdd3bf1b716
“ashomidi” <ashkan@hamrahkish.com>
2023-10-08 10:58:31 +0330

    spinners added

--- assets/js/aumchart.js
+++ assets/js/aumchart.js
@@ -1,51 +1,60 @@
+const aum_spinner = document.getElementById("aum-chart-spinner");
+
 const aumChartOptions = {
-    height: 200,
-    layout: {
-        background: { color: 'transparent' },
-        textColor: '#DDD',
-    },
-    grid: {
-        vertLines: { color: '#2b2b2b' },
-        horzLines: { color: '#2b2b2b' },
-    },
-    timeScale: {
-        timeVisible: true,
-        secondsVisible: false,
-    },
-}
+  height: 200,
+  layout: {
+    background: { color: "transparent" },
+    textColor: "#DDD",
+  },
+  grid: {
+    vertLines: { color: "#2b2b2b" },
+    horzLines: { color: "#2b2b2b" },
+  },
+  timeScale: {
+    timeVisible: true,
+    secondsVisible: false,
+  },
+};
 
 const updateAUMChartData = async () => {
-    const selectedTimeFrame = document.getElementById('timeFrameSelectForAUM').value;
-    const totalFlwsWrapper = document.getElementById('total-followers-item');
+  aum_spinner.classList.remove("hidden");
+  const selectedTimeFrame = document.getElementById(
+    "timeFrameSelectForAUM"
+  ).value;
+  const totalFlwsWrapper = document.getElementById("total-followers-item");
 
-    try {
-        const response = await fetch(`https://master.soroosh.app/api/report/chart/master/godofweb3?format=json&report=FOLLOWERS&time_frame=${selectedTimeFrame}`);
-        const data = await response.json();
+  try {
+    const response = await fetch(
+      `https://master.soroosh.app/api/report/chart/master/godofweb3?format=json&report=FOLLOWERS&time_frame=${selectedTimeFrame}`
+    );
+    const data = await response.json();
 
-        totalFlwsWrapper.innerHTML = data.total_followers;
+    aum_spinner.classList.add("hidden");
+    totalFlwsWrapper.innerHTML = data.total_followers;
 
-        const chartData = data.data.map(item => ({
-            time: item.timestamp / 1000,
-            value: item.value,
-        }));
+    const chartData = data.data.map((item) => ({
+      time: item.timestamp / 1000,
+      value: item.value,
+    }));
 
-        if (chartAum) {
-            chartAum.remove();
-        }
-
-        const container = document.getElementById('chart-container2');
-        chartAum = LightweightCharts.createChart(container, aumChartOptions);
-
-        const series = chartAum.addAreaSeries();
-        series.setData(chartData);
-        chartAum.timeScale().fitContent();
-
-    } catch (error) {
-        console.error('Error fetching or rendering data:', error);
+    if (chartAum) {
+      chartAum.remove();
     }
+
+    const container = document.getElementById("aum-chart-container");
+    chartAum = LightweightCharts.createChart(container, aumChartOptions);
+
+    const series = chartAum.addAreaSeries();
+    series.setData(chartData);
+    chartAum.timeScale().fitContent();
+  } catch (error) {
+    console.error("Error fetching or rendering data:", error);
+  }
 };
 
 let chartAum;
 
-document.getElementById('timeFrameSelectForAUM').addEventListener('change', updateAUMChartData);
+document
+  .getElementById("timeFrameSelectForAUM")
+  .addEventListener("change", updateAUMChartData);
 updateAUMChartData();
--- assets/js/flchart.js
+++ assets/js/flchart.js
@@ -1,51 +1,60 @@
+const flws_spinner = document.getElementById("flws-chart-spinner");
+
 const chartOptions = {
-    height: 200,
-    layout: {
-        background: { color: 'transparent' },
-        textColor: '#DDD',
-    },
-    grid: {
-        vertLines: { color: '#2b2b2b' },
-        horzLines: { color: '#2b2b2b' },
-    },
-    timeScale: {
-        timeVisible: true,
-        secondsVisible: false,
-    },
-}
+  height: 200,
+  layout: {
+    background: { color: "transparent" },
+    textColor: "#DDD",
+  },
+  grid: {
+    vertLines: { color: "#2b2b2b" },
+    horzLines: { color: "#2b2b2b" },
+  },
+  timeScale: {
+    timeVisible: true,
+    secondsVisible: false,
+  },
+};
 
 const updateChartData = async () => {
-    const selectedTimeFrame = document.getElementById('timeFrameSelectForflws').value;
-    const totalFlwsWrapper = document.getElementById('total-followers-item');
+  flws_spinner.classList.remove("hidden");
+  const selectedTimeFrame = document.getElementById(
+    "timeFrameSelectForflws"
+  ).value;
+  const totalFlwsWrapper = document.getElementById("total-followers-item");
 
-    try {
-        const response = await fetch(`https://master.soroosh.app/api/report/chart/master/godofweb3?format=json&report=FOLLOWERS&time_frame=${selectedTimeFrame}`);
-        const data = await response.json();
+  try {
+    const response = await fetch(
+      `https://master.soroosh.app/api/report/chart/master/godofweb3?format=json&report=FOLLOWERS&time_frame=${selectedTimeFrame}`
+    );
+    const data = await response.json();
+    flws_spinner.classList.add("hidden");
 
-        totalFlwsWrapper.innerHTML = data.total_followers;
+    totalFlwsWrapper.innerHTML = data.total_followers;
 
-        const chartData = data.data.map(item => ({
-            time: item.timestamp / 1000,
-            value: item.value,
-        }));
+    const chartData = data.data.map((item) => ({
+      time: item.timestamp / 1000,
+      value: item.value,
+    }));
 
-        if (chart) {
-            chart.remove();
-        }
-
-        const container = document.getElementById('chart-container');
-        chart = LightweightCharts.createChart(container, chartOptions);
-
-        const series = chart.addAreaSeries();
-        series.setData(chartData);
-        chart.timeScale().fitContent();
-
-    } catch (error) {
-        console.error('Error fetching or rendering data:', error);
+    if (chart) {
+      chart.remove();
     }
+
+    const container = document.getElementById("flws-chart-container");
+    chart = LightweightCharts.createChart(container, chartOptions);
+
+    const series = chart.addAreaSeries();
+    series.setData(chartData);
+    chart.timeScale().fitContent();
+  } catch (error) {
+    console.error("Error fetching or rendering data:", error);
+  }
 };
 
 let chart;
 
-document.getElementById('timeFrameSelectForflws').addEventListener('change', updateChartData);
+document
+  .getElementById("timeFrameSelectForflws")
+  .addEventListener("change", updateChartData);
 updateChartData();
